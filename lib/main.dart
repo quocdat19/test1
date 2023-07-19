faf751db1ebb6c62a02e965ab1d37f4b2b8a0514
unknown <quocdat19092000@gmail.com>
2023-07-19 17:08:13 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+//1. Khai bao import  thu vien
+  import 'package:flutter/material.dart';
+
+//2. Main
+void main() {
+
+  //3. runApp: khoi dong app
+  runApp(const MaterialApp(
+  //4. Su dung thanh pham de xay dung giao dien nguoi dung
+    home: Text('Hellooooooo Dat'),
+  ));
+}
+
+
+  
